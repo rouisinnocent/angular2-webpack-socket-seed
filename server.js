a613b542aafb79d8ad8d8109d4c79a34c22e9daf
Ezequiel Garrido <Ezequiel Garrido>
2016-08-16 22:48:06 -0300

    Make dist/ public.

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const app = express();
 
 const path = require('path');
 const indexPath = path.resolve(__dirname, 'dist', 'index.html');
+const distPath = path.resolve(__dirname, 'dist');
 
 const config = require('./config.json');
 const port = config.port;
+
+app.use(
+    '/dist',
+    express.static(distPath)
+)
 
 app.get('/', (req, res) => {
     res.sendFile(indexPath);
